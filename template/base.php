33bf7988457bac2b354183cebeac02bfb2d531e2
Elisa Yan <elisa.yan@studio.unibo.it>
2025-01-20 17:22:05 +0100

    implemented footer

---
+++ template/base.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>TrainTrack - Home</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+
+<body>
+    <header>
+        <h1>TrainTrack</h1>
+    </header>
+    
+    <footer>
+        <a href="chi-siamo.php">Chi siamo</a>
+        <a href="https://www.facebook.com/"><img src="./img/facebook.png" alt="Facebook"></a>
+        <a href="https://www.instagram.com/"><img src="./img/instagram.png" alt="Instagram"></a>
+        <a href="https://x.com/"><img src="./img/x.png" alt="X"></a>
+        <a href="https://www.linkedin.com/"><img src="./img/linkedin.png" alt="LinkedIn"></a>
+        <a href="https://www.youtube.com/"><img src="./img/youtube.png" alt="Youtube"></a>
+    </footer>
+</body>
+</html>
