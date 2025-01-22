c6aa50b4915ca9897287bbbbba645340a3bdcf3f
Elisa Yan <elisa.yan@studio.unibo.it>
2025-01-22 16:33:29 +0100

    implementing base interna

--- template/base-interna.php
+++ template/base-interna.php
@@ -3,15 +3,27 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>TrainTrack</title>
-    <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    <link rel="stylesheet" type="text/css" href="./css/style-interna.css" />
 </head>
 
 <nav>
     <ul>
-        <li><a href="home.php"><img src="" alt=""></a></li>
-        <li><a href="login.php"><img src="" alt=""></a></li>
-        <li><a href="carrello.php"><img src="" alt=""></a></li>
+        <div>
+            <li><a href="home.php"><img src="./img/home.png" alt="Home"></a></li>
+            <li><a href="login.php"><img src="./img/user.png" alt="User"></a></li>
+        </div>
+        <div>
+            <li><a href="carrello.php"><img src="./img/cart.png" alt="Cart"></a></li>
+        </div>
     </ul>
 </nav>
+
+<body>
+    <footer>
+        <ul>
+            <li><a href="help.php"><img src="./img/help.png" alt="Help"></a></li>
+        </ul>
+    </footer>
+</body>
 
 </html>
