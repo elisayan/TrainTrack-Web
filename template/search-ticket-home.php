8ad3a4ee0fc0be187b85b30feea223031ce8b758
Amiela P Junio <amielap.junio@studio.unibo.it>
2025-01-30 16:24:12 +0100

    fixed contents position

--- template/search-ticket-home.php
+++ template/search-ticket-home.php
@@ -43,21 +43,33 @@
                     
 
                             <div class="col-md-6 col-lg-6">
-                                <div class="row">
+                                <div class="row adult justify-content-end">
                                     <div class="col-auto passenger">
                                         <label for="adult"><img src="./img/user.png" alt="adulto"></label>
-                                        <button><img src="./img/minus.png" alt="meno"></button>
+                                    </div>
+                                    <div class="col-auto passenger">
+                                        <button class="passenger"><img src="./img/minus.png" alt="meno"></button>
+                                    </div>
+                                    <div class="col-auto passenger">
                                         <input type="text" value="1" id="adult">
-                                        <button><img src="./img/more.png" alt="più"></button>
+                                    </div>
+                                    <div class="col-auto passenger">
+                                        <button class="passenger"><img src="./img/more.png" alt="più"></button>
                                     </div>
                                 </div>
 
-                                <div class="row">
+                                <div class="row kid justify-content-end">
                                     <div class="col-auto passenger">
                                         <label class="kid" for="kid"><img src="./img/user.png" alt="bambino"></label>
-                                        <button><img src="./img/minus.png" alt="meno"></button>                            
-                                        <input type="text" value="1" id="kid">                            
-                                        <button><img src="./img/more.png" alt="più"></button>
+                                    </div>
+                                    <div class="col-auto passenger">
+                                        <button class="passenger"><img src="./img/minus.png" alt="meno"></button>                            
+                                    </div>
+                                    <div class="col-auto passenger">
+                                        <input type="text" value="0" id="kid">                            
+                                    </div>
+                                    <div class="col-auto passenger">
+                                        <button class="passenger"><img src="./img/more.png" alt="più"></button>
                                     </div>
                                 </div>
                             </div>
@@ -67,7 +79,7 @@
                 
                 <div class="row">
                     <div class="col-auto me-auto">
-                        <div class="row">
+                        <div class="row service">
                             <p>Servizi Complementari:</p>
                         </div>
                         <div class="form-check">
