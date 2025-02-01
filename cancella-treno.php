86b66f3a2d5568a2abecb6b28acc4f9b9260f455
Elisa Yan <elisa.yan@studio.unibo.it>
2025-02-01 22:29:41 +0100

    implemented template delete train

---
+++ cancella-treno.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'bootstrap.php';
+
+$templateParams["titolo"]="TrainTrack - Profilo Macchinista";
+$templateParams["nome"]="template/profilo-macchinista.php";
+$templateParams["azione"] = "template/cancella-treno.php";
+
+require 'template/base.php';
+?>
