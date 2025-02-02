3bc197272f74cabd8ddcd5151916c0b06bfa07b2
Elisa Yan <elisa.yan@studio.unibo.it>
2025-02-02 20:50:00 +0100

    fixed margin

--- template/home.php
+++ template/home.php
@@ -1,58 +1,60 @@
-<div class="row text-center">
-    <div class="col-12 mb-4">
+<div class="container-fluid text-center">
+    <div class="col-12 mb-3">
         <h2>Benvenuto su TrainTrack</h2>
         <p>Prenota i tuoi biglietti, gestisci abbonamenti e controlla i tuoi ordini</p>
     </div>
 
-    <div class="col-6 col-md-3 mb-4">
-        <a href="biglietti.php" class="card-link">
-            <div class="card h-100 shadow hover-effect">
-                <img src="./img/ticket.png" class="card-img-top p-3 img-card-custom" alt="Biglietti">
-                <div class="card-body">
-                    <h5 class="card-title">Biglietti</h5>
-                    <p class="text-muted small">Acquista i tuoi biglietti</p>
+    <div class="row">
+        <div class="col-6 col-md-3 mb-3">
+            <a href="biglietti.php" class="card-link">
+                <div class="card h-100 shadow hover-effect">
+                    <img src="./img/ticket.png" class="card-img-top p-3 img-card-custom" alt="Biglietti">
+                    <div class="card-body">
+                        <h5 class="card-title">Biglietti</h5>
+                        <p class="text-muted small">Acquista i tuoi biglietti</p>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
+
+        <div class="col-6 col-md-3 mb-3">
+            <a href="abbonamenti.php" class="card-link">
+                <div class="card h-100 shadow hover-effect">
+                    <img src="./img/subscription.png" class="card-img-top p-3 img-card-custom" alt="Abbonamenti">
+                    <div class="card-body">
+                        <h5 class="card-title">Abbonamenti</h5>
+                        <p class="text-muted small">Scopri le nostre offerte</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-6 col-md-3 mb-3">
+            <a href="login.php" class="card-link">
+                <div class="card h-100 shadow hover-effect">
+                    <img src="./img/profile.png" class="card-img-top p-3 img-card-custom" alt="Profilo">
+                    <div class="card-body">
+                        <h5 class="card-title">Profilo</h5>
+                        <p class="text-muted small">Gestisci il tuo account</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-6 col-md-3 mb-3">
+            <a href="ordini.php" class="card-link">
+                <div class="card h-100 shadow hover-effect">
+                    <img src="./img/path.png" class="card-img-top p-3 img-card-custom" alt="Ordini">
+                    <div class="card-body">
+                        <h5 class="card-title">Percorsi</h5>
+                        <p class="text-muted small">Visualizza i tuoi percorsi</p>
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 
-    <div class="col-6 col-md-3 mb-4">
-        <a href="abbonamenti.php" class="card-link">
-            <div class="card h-100 shadow hover-effect">
-                <img src="./img/subscription.png" class="card-img-top p-3 img-card-custom" alt="Abbonamenti">
-                <div class="card-body">
-                    <h5 class="card-title">Abbonamenti</h5>
-                    <p class="text-muted small">Scopri le nostre offerte</p>
-                </div>
-            </div>
-        </a>
-    </div>
-
-    <div class="col-6 col-md-3 mb-4">
-        <a href="login.php" class="card-link">
-            <div class="card h-100 shadow hover-effect">
-                <img src="./img/profile.png" class="card-img-top p-3 img-card-custom" alt="Profilo">
-                <div class="card-body">
-                    <h5 class="card-title">Profilo</h5>
-                    <p class="text-muted small">Gestisci il tuo account</p>
-                </div>
-            </div>
-        </a>
-    </div>
-
-    <div class="col-6 col-md-3 mb-4">
-        <a href="ordini.php" class="card-link">
-            <div class="card h-100 shadow hover-effect">
-                <img src="./img/path.png" class="card-img-top p-3 img-card-custom" alt="Ordini">
-                <div class="card-body">
-                    <h5 class="card-title">Percorsi</h5>
-                    <p class="text-muted small">Visualizza i tuoi percorsi</p>
-                </div>
-            </div>
-        </a>
-    </div>
-
-    <div class="col-12 mb-4">
+    <div class="col-12 mb-3">
         <div class="alert alert-info">
             <h4>Promozioni Attive</h4>
             <p>Scopri le nostre offerte speciali per viaggi frequenti!</p>
