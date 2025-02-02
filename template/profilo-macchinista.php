9259048f2366907f043ac62f5ad04bec6822ca9f
Elisa Yan <elisa.yan@studio.unibo.it>
2025-02-02 15:55:48 +0100

    deleted useless function

--- template/profilo-macchinista.php
+++ template/profilo-macchinista.php
@@ -3,7 +3,6 @@
         <h4>Azioni</h4>
         <a href="aggiungi-percorso.php">Aggiungi Percorso</a>
         <a href="cambia-orario.php">Cambia Orario</a>
-        <a href="cancella-treno.php">Cancella Treno</a>
     </div>
 
     <div class="content">
