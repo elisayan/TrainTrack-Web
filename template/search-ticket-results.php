2f7c37a7c92614186554b5d25becf6395752f4d4
Amiela P Junio <amielap.junio@studio.unibo.it>
2025-02-05 22:56:25 +0100

    fixed search header

--- template/search-ticket-results.php
+++ template/search-ticket-results.php
@@ -1,39 +1,39 @@
 <section>
     <header>
         <div class="row searchheader">
-            <div class="col-5">
+            <div class="col-md-7 col-lg-5">
                 <div class="row">
-                    <div class="col">
+                    <div class="col-auto">
                         <p><?php echo $departureStation; ?></p>
                     </div>
-                    <div class="col">
+                    <div class="col-auto">
                         <img src="./img/next.png" alt="">
                     </div>
-                    <div class="col">
+                    <div class="col-auto">
                         <p><?php echo $destinationStation; ?></p>
                     </div>            
                 </div>
             </div>
-            <div class="col-5">
-                <div class="row">
-                    <div class="col">
-                        <img src="./img/calendar.png" alt="">
-                    </div>
-                    <div class="col">
-                        <p><?php echo $departureDate; ?></p>
-                    </div>
-                    <div class="col">
-                        <p><?php echo $departureTime; ?></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2">
+            <div class="col-md-5 col-lg-2">
                 <div class="row">
                     <div class="col">
                         <img src="./img/ticket.png" alt="">
                     </div>
                     <div class="col">
                         <p><?php echo $numberTickets; ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 col-lg-5">
+                <div class="row">
+                    <div class="col-auto">
+                        <img src="./img/calendar.png" alt="">
+                    </div>
+                    <div class="col-auto">
+                        <p><?php echo $departureDate; ?></p>
+                    </div>
+                    <div class="col-auto">
+                        <p><?php echo $departureTime; ?></p>
                     </div>
                 </div>
             </div>
