8a360c863c03b1ebed799274a37319739d112383
Amiela P Junio <amielap.junio@studio.unibo.it>
2025-04-28 17:42:26 +0200

    changed search ticket layout and added carousel with 3 sample img

--- template/search-ticket-home.php
+++ template/search-ticket-home.php
@@ -1,75 +1,77 @@
 <section class="search-ticket-section">
-    <form class="card shadow p-2. mt-5 mb-5" action="search-tickets-results.php" method="GET">
-        <?php if (isset($templateParams["errore_ricerca_biglietto"])): ?>
-            <p class="error"><?php echo $templateParams["errore_ricerca_biglietto"]; ?></p>
-        <?php endif; ?>
-        <div class="container justify-content-center">
-            <div class="row justify-content-center">
-                <div class="row station">
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <form class="card shadow p-3 mb-5" action="search-tickets-results.php" method="GET">
+            <?php if (isset($templateParams["errore_ricerca_biglietto"])): ?>
+                <p class="error"><?php echo $templateParams["errore_ricerca_biglietto"]; ?></p>
+            <?php endif; ?>
+                <div class="row justify-content-center">
+                    <div class="row station">
                     
-                    <div class="col-md-12 col-lg-3">
-                        <label class="station ticket" for="stazione_partenza">Partenza:</label>
-                        <input class="form-control" list="datalistStation" id="stazione_partenza" name="stazione_partenza" required />
-                        <datalist id="datalistStation">
+                        <div class="col-md-12">
+                            <label class="station ticket" for="stazione_partenza">Partenza:</label>
+                            <input class="form-control" list="datalistStation" id="stazione_partenza" name="stazione_partenza" required />
+                            <datalist id="datalistStation">
                             <?php foreach ($templateParams["nome_stazioni"] as $stazione): ?>
                                 <option value="<?php echo $stazione["nome_stazioni"]; ?>">
                             <?php endforeach; ?>
-                        </datalist>
-                    </div>
+                            </datalist>
+                        </div>
 
                     
-                    <div class="col-md-12 col-lg-3">
-                        <label class="station ticket" for="stazione_arrivo">Arrivo:</label>
-                        <input class="form-control" list="datalistStation" id="stazione_arrivo" name="stazione_arrivo" required />
-                        <datalist id="datalistStation">
+                        <div class="col-md-12">
+                            <label class="station ticket" for="stazione_arrivo">Arrivo:</label>
+                            <input class="form-control" list="datalistStation" id="stazione_arrivo" name="stazione_arrivo" required />
+                            <datalist id="datalistStation">
                             <?php foreach ($templateParams["nome_stazioni"] as $stazione): ?>
                                 <option value="<?php echo $stazione["nome_stazioni"]; ?>">
                             <?php endforeach; ?>
-                        </datalist>
-                    </div>
+                            </datalist>
+                        </div>
                     
                     
-                    <div class="col-md-12 col-lg-6">
-                        <div class="row justify-content-center">
-                            <div class="col-md-6 col-lg-6">
-                                <div class="row">
+                        <div class="col-md-12">
+                            <div class="row justify-content-center">
+                                <div class="col-md-6 col-lg-6">
+                                    <div class="row">
                                     <input class="form-control" type="date" id="data_partenza" name="data_partenza" required>
-                                </div>
-                                <div class="row">
-                                    <input class="form-control" type="time" id="orario_partenza" name="orario_partenza" required>
-                                </div>
-                            </div>
-
-                            <div class="col-md-6 col-lg-6">
-                                
-                                <div class="row adult justify-content-end">
-                                    <div class="col-auto passenger">
-                                        <label for="numero_biglietti_adulti"><img src="./img/user.png" alt="Adult Icon"></label>
                                     </div>
-                                    <div class="col-auto passenger">
-                                        <button type="button" class="passenger" onclick="updateTickets('numero_biglietti_adulti', -1)"><img src="./img/minus.png" alt="Decrease"></button>
-                                    </div>
-                                    <div class="col-auto passenger">
-                                        <input type="text" value="1" id="numero_biglietti_adulti" name="numero_biglietti_adulti" readonly>
-                                    </div>
-                                    <div class="col-auto passenger">
-                                        <button type="button" class="passenger" onclick="updateTickets('numero_biglietti_adulti', 1)"><img src="./img/more.png" alt="Increase"></button>
+                                    <div class="row">
+                                        <input class="form-control" type="time" id="orario_partenza" name="orario_partenza" required>
                                     </div>
                                 </div>
 
+                                <div class="col-md-6 col-lg-6">
                                 
-                                <div class="row kid justify-content-end">
-                                    <div class="col-auto passenger">
-                                        <label class="kid" for="numero_biglietti_bambini"><img src="./img/user.png" alt="Child Icon"></label>
+                                    <div class="row adult justify-content-end">
+                                        <div class="col-auto passenger">
+                                            <label for="numero_biglietti_adulti"><img src="./img/user.png" alt="Adult Icon"></label>
+                                        </div>
+                                        <div class="col-auto passenger">
+                                            <button type="button" class="passenger" onclick="updateTickets('numero_biglietti_adulti', -1)"><img src="./img/minus.png" alt="Decrease"></button>
+                                        </div>
+                                        <div class="col-auto passenger">
+                                            <input type="text" value="1" id="numero_biglietti_adulti" name="numero_biglietti_adulti" readonly>
+                                        </div>
+                                        <div class="col-auto passenger">
+                                            <button type="button" class="passenger" onclick="updateTickets('numero_biglietti_adulti', 1)"><img src="./img/more.png" alt="Increase"></button>
+                                        </div>
                                     </div>
-                                    <div class="col-auto passenger">
-                                        <button type="button" class="passenger" onclick="updateTickets('numero_biglietti_bambini', -1)"><img src="./img/minus.png" alt="Decrease"></button>
-                                    </div>
-                                    <div class="col-auto passenger">
-                                        <input type="text" value="0" id="numero_biglietti_bambini" name="numero_biglietti_bambini" readonly>
-                                    </div>
-                                    <div class="col-auto passenger">
-                                        <button type="button" class="passenger" onclick="updateTickets('numero_biglietti_bambini', 1)"><img src="./img/more.png" alt="Increase"></button>
+
+                                
+                                    <div class="row kid justify-content-end">
+                                        <div class="col-auto passenger">
+                                            <label class="kid" for="numero_biglietti_bambini"><img src="./img/user.png" alt="Child Icon"></label>
+                                        </div>
+                                        <div class="col-auto passenger">
+                                            <button type="button" class="passenger" onclick="updateTickets('numero_biglietti_bambini', -1)"><img src="./img/minus.png" alt="Decrease"></button>
+                                        </div>
+                                        <div class="col-auto passenger">
+                                            <input type="text" value="0" id="numero_biglietti_bambini" name="numero_biglietti_bambini" readonly>
+                                        </div>
+                                        <div class="col-auto passenger">
+                                            <button type="button" class="passenger" onclick="updateTickets('numero_biglietti_bambini', 1)"><img src="./img/more.png" alt="Increase"></button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -77,8 +79,8 @@
                     </div>
                 </div>
 
-                
-                <div class="row">
+                <div class="row justify-content-center">
+                <div class="row service">
                     <div class="col-auto me-auto">
                         <div class="row row-service">
                             <p>Servizi Complementari:</p>
@@ -96,15 +98,41 @@
                 </div>
 
                 
-                <div class="row">
+                <div class="row button-search">
                     <div class="col-auto me-auto"></div>
                     <div class="col-auto">
                         <input class="btn btn-primary btn-sm btn-search" type="submit" name="submit" value="CERCA">
                     </div>
                 </div>
             </div>
+        </form>
         </div>
-    </form>
+    
+        <div class="col-12 col-md-6">
+            <div id="carouselExampleIndicators" class="carousel slide mb-5" data-bs-ride="carousel">
+
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="./img/blue.png" class="d-block w-100 img-carousel" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./img/red.png" class="d-block w-100 img-carousel" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="./img/green.png" class="d-block w-100 img-carousel" alt="...">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </a>
+            </div>
+            </div>
+        </div>
+
+    </div>
 </section>
 
 <script>
