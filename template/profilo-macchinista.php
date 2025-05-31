f94e6aefec4101c6b13bfa33a191a6dd12b9cfb4
Elisa Yan <elisa.yan@studio.unibo.it>
2025-05-31 13:35:23 +0200

    configured in mobile first view

--- template/profilo-macchinista.php
+++ template/profilo-macchinista.php
@@ -1,22 +1,70 @@
-<div class="container">
-    <div class="sidebar">
-        <h4><a href="profilo-macchinista.php">Azioni</a></h4>
-        <a href="aggiungi-percorso.php">Aggiungi Percorso</a>
-        <a href="cambia-orario.php">Cambia Orario</a>
-    </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 d-md-none py-2">
+            <button
+                class="btn btn-outline-primary"
+                type="button"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#sidebarOffcanvas"
+                aria-controls="sidebarOffcanvas"
+            >
+                ☰ Menu
+            </button>
+        </div>
 
-    <div class="content">
-    <?php if(isset($templateParams["azione"])): ?>
-            <?php require($templateParams["azione"]); ?>
-        <?php else: ?>
-            <div class="welcome-message">
-                <h3>Benvenuto, <?php echo $user[0]["Nome"]; ?>!</h3>
-                <p>Da questa dashboard puoi:</p>
-                <ul class="welcome-list">
-                    <li><a href="aggiungi-percorso.php">Creare percorsi ferroviari</a></li>
-                    <li><a href="cambia-orario.php">Gestire gli orari dei treni</a></li>
+        <div class="col-md-3">
+            <div class="collapse d-md-block" id="sidebarCollapse">
+                <div class="bg-light rounded p-3 h-100 sidebar">
+                    <h4 class="mb-3"><a href="profilo-macchinista.php">Azioni</a></h4>
+                    <nav class="nav flex-column">
+                        <a class="nav-link" href="aggiungi-percorso.php">Aggiungi Percorso</a>
+                        <a class="nav-link" href="cambia-orario.php">Cambia Orario</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+        <div
+            class="offcanvas offcanvas-start d-md-none"
+            tabindex="-1"
+            id="sidebarOffcanvas"
+            aria-labelledby="sidebarOffcanvasLabel"
+        >
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="sidebarOffcanvasLabel">Azioni</h5>
+                <button
+                    type="button"
+                    class="btn-close text-reset"
+                    data-bs-dismiss="offcanvas"
+                    aria-label="Chiudi"
+                ></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2">
+                        <a class="nav-link" href="aggiungi-percorso.php">Aggiungi Percorso</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cambia-orario.php">Cambia Orario</a>
+                    </li>
                 </ul>
             </div>
-        <?php endif; ?>
+        </div>
+
+        <div class="col-12 col-md-9">
+            <?php if (isset($templateParams["azione"])): ?>
+                <?php require($templateParams["azione"]); ?>
+            <?php else: ?>
+                <div class="welcome-message p-3">
+                    <h3>Benvenuto, <?= htmlspecialchars($user[0]["Nome"]) ?>!</h3>
+                    <p>Da questa dashboard puoi:</p>
+                    <ul class="welcome-list">
+                        <li><a href="aggiungi-percorso.php">Creare percorsi ferroviari</a></li>
+                        <li><a href="cambia-orario.php">Gestire gli orari dei treni</a></li>
+                    </ul>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
+
