a0135b4963d361c9406a0109760b91c4593f600c
Elisa Yan <elisa.yan@studio.unibo.it>
2025-06-03 11:37:19 +0200

    fix img path

--- home.php
+++ home.php
@@ -3,7 +3,6 @@ require_once 'bootstrap.php';
 
 $templateParams['titolo'] = "TrainTrack - Home";
 $templateParams['nome'] = "template/home.php";
-$templateParams['cardImage'] = 'path.png';
 
 require 'template/base.php';
 ?>
