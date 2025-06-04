8eb3f0a84c6b270c652871534f8932c35d1a080a
Elisa Yan <elisa.yan@studio.unibo.it>
2025-06-04 16:48:22 +0200

    fix <i>

--- template/notifica.php
+++ template/notifica.php
@@ -6,28 +6,21 @@
     <?php else: ?>
         <div class="list-group">
             <?php foreach ($notifiche as $notifica): ?>
-                <form method="POST" class="list-group-item d-flex justify-content-between align-items-center rounded-3 shadow-sm <?= $notifica['Letto'] ? 'letta' : '' ?>">
+                <form method="POST"
+                    class="list-group-item d-flex justify-content-between align-items-center rounded-3 shadow-sm <?= $notifica['Letto'] ? 'letta' : '' ?>">
 
                     <input type="hidden" name="id_notifica" value="<?= $notifica['CodNotifica'] ?>">
-                    
+
                     <div class="form-check">
-                        <input type="checkbox" 
-                               class="form-check-input" 
-                               id="notifica-<?= $notifica['CodNotifica'] ?>" 
-                               onchange="this.form.submit()" 
-                               name="azione" 
-                               value="segna_letta" 
-                               <?= $notifica['Letto'] ? 'checked disabled' : '' ?>>
+                        <input type="checkbox" class="form-check-input" id="notifica-<?= $notifica['CodNotifica'] ?>"
+                            onchange="this.form.submit()" name="azione" value="segna_letta" <?= $notifica['Letto'] ? 'checked disabled' : '' ?>>
                         <label class="form-check-label" for="notifica-<?= $notifica['CodNotifica'] ?>">
                             <?= htmlspecialchars($notifica['Descrizione']) ?>
                         </label>
                     </div>
 
-                    <button type="submit" 
-                            name="azione" 
-                            value="cancella" 
-                            class="btn btn-link text-danger p-0">
-                        <i class="bi bi-x-lg"></i>
+                    <button type="submit" name="azione" value="cancella" class="btn btn-link text-danger p-0">
+                        <span class="bi bi-x-lg" aria-hidden="true"></span>
                     </button>
                 </form>
             <?php endforeach; ?>
